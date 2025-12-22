79a774140effa47187df0555cbb11d0b92e22975
Binada <binada2005@gmail.com>
2025-12-22 13:39:25 +0530

    Search function added
    I added a seperate JS fucntion to implement the search logic

---
+++ interface/src/utils/searchProperties.js
@@ -0,0 +1,58 @@
+// Converts the provided object into a JS date
+const toDate = (date) => {
+  return new Date(
+    date.year,
+    new Date(`${date.month} 1, 2000`).getMonth(),
+    date.day
+  );
+};
+
+export const searchProperties = (properties, filters) => {
+  return properties.filter((property) => {
+    if (filters.type && filters.type !== "Any") {
+      if (property.type !== filters.type) {
+        return false;
+      }
+    }
+
+    if (filters.minPrice) {
+      if (property.price < filters.minPrice) {
+        return false;
+      }
+    }
+
+    if (filters.maxPrice) {
+      if (property.price > filters.maxPrice) {
+        return false;
+      }
+    }
+
+    if (filters.minBedrooms) {
+      if (property.bedrooms < filters.minBedrooms) {
+        return false;
+      }
+    }
+
+    if (filters.maxBedrooms) {
+      if (property.bedrooms > filters.maxBedrooms) {
+        return false;
+      }
+    }
+
+    if (filters.postcode) {
+      const propertyPostcode = property.location.split(" ").pop();
+      if (!propertyPostcode.startsWith(filters.postcode.toUpperCase())) {
+        return false;
+      }
+    }
+
+    if (filters.dateFrom) {
+      const propertyDate = toDate(property.added);
+      if (propertyDate < filters.dateFrom) {
+        return false;
+      }
+    }
+
+    return true;
+  });
+};
